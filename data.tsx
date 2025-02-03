0538618af0c68c7226556595900548c758fb130e
keinner <foreverkeinner@gmail.com>
2025-02-03 12:51:49 -0400

    first commit

---
+++ data.tsx
@@ -0,0 +1,166 @@
+import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+
+export const socialNetworks = [
+    {
+        id: 2,
+        logo: <Linkedin size={30} strokeWidth={1} />,
+        src: "https://www.linkedin.com/in/keinner-frontado-480620321/",
+    },
+    {
+        id: 3,
+        logo: <Twitter size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+];
+
+
+export const itemsNavbar = [
+    {
+        id: 1,
+        title: "Home",
+        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "User",
+        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+        link: "/about-me",
+    },
+    {
+        id: 3,
+        title: "Book",
+        icon: <BookText size={25} color="#fff" strokeWidth={1} />,
+        link: "/services",
+    },
+    {
+        id: 4,
+        title: "Target",
+        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+        link: "/portfolio",
+    },
+    {
+        id: 5,
+        title: "Home",
+        icon: <Speech size={25} color="#fff" strokeWidth={1} />,
+        link: "/testimonials",
+    },
+];
+
+export const dataAboutPage = [
+    {
+        id: 2,
+        title: "Creador de Experiencias Digitales",
+        subtitle: "PixelCrafters",
+        description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
+        date: "May 2024",
+    },
+]
+
+export const dataCounter = [
+    {
+        id: 0,
+        endCounter: 2,
+        text: "Años de experiencia",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 1,
+        endCounter: 3,
+        text: "Tiempo de estudio",
+        lineRight: true,
+        lineRightMobile: false,
+    },
+    {
+        id: 2,
+        endCounter: 20,
+        text: "Proyectos finalizados",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+];
+
+export const serviceData = [
+    {
+        icon: <Crop />,
+        title: "Branding",
+        description: "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
+    },
+    {
+        icon: <Pencil />,
+        title: "Diseño web",
+        description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+    },
+    {
+        icon: <Computer />,
+        title: "Desarrollo web",
+        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+    },
+    {
+        icon: <Book />,
+        title: "Copywriting",
+        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
+    },
+    {
+        icon: <Rocket />,
+        title: "SEO",
+        description: "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
+    },
+];
+
+export const dataPortfolio = [
+    {
+        id: 1,
+        title: "E-comerce",
+        image: "/image-8.jpg",
+        urlGithub: "https://github.com/keinner-dev/E-comerce",
+        urlDemo: "#!",
+    },
+    {
+        id: 2,
+        title: "Ichiban Motorcycle",
+        image: "/image-2.jpg",
+        urlGithub: "https://github.com/keinner-dev/Motos",
+        urlDemo: "https://motorsky.netlify.app/",
+    },
+    {
+        id: 3,
+        title: "Plants Shop",
+        image: "/image-3.jpg",
+        urlGithub: "https://github.com/keinner-dev/Page-Plant",
+        urlDemo: "https://plantstoreikay.netlify.app/",
+    },
+    {
+        id: 4,
+        title: "Tienda de comida",
+        image: "/image-4.jpg",
+        urlGithub: "https://github.com/keinner-dev/FOOD",
+        urlDemo: "https://fastfood-keinner.netlify.app/",
+    },
+
+];
+
+export const dataTestimonials = [
+    {
+        id: 1,
+        name: "George Snow",
+        description:
+            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+        imageUrl: "/profile1.png",
+    },
+    {
+        id: 2,
+        name: "Juan Pérez",
+        description:
+            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
+        imageUrl: "/profile2.png",
+    },
+    {
+        id: 3,
+        name: "María García",
+        description:
+            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
+        imageUrl: "/profile3.png",
+    },
+];
