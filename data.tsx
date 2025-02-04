7313f268f875dddde3ae234859d9a546718fcdfb
keinner-dev <foreverkeinner@gmail.com>
2025-02-04 14:18:58 -0400

    Add files via upload

--- data.tsx
+++ data.tsx
@@ -83,11 +83,6 @@ export const dataCounter = [
 
 export const serviceData = [
     {
-        icon: <Crop />,
-        title: "Branding",
-        description: "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
-    },
-    {
         icon: <Pencil />,
         title: "Diseño web",
         description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
@@ -96,11 +91,6 @@ export const serviceData = [
         icon: <Computer />,
         title: "Desarrollo web",
         description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
-    },
-    {
-        icon: <Book />,
-        title: "Copywriting",
-        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
     },
     {
         icon: <Rocket />,
